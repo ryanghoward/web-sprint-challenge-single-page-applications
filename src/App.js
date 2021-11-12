2c0ddc3950020ecdb87e2c0e6236d31c92ab60e5
Ryan Howard <ryanghoward@protonmail.com>
2021-11-12 09:05:45 -0800

    Home.js set up and imported to App.js. Pizza image and place order button redering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
 
 const App = () => {
   return (
     <>
+      <Route>
+        <Home />
+      </Route>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import pizza from "./Assets/Pizza.jpg";
+
+function Home() {
+  return (
+    <div className='home-wrapper'>
+      <img className='home-image' src={pizza} alt='Pizza' />
+      <Link to='/pizza'>
+        <button className='order-button'>Place order</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
